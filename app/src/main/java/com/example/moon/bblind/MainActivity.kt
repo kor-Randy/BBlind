373b353ea4d5f38beaa9eecfebc5490db8ccfc36
wlgusdn <randy_95@naver.com>
2019-04-19 16:58:10 +0900

    FireBase Setting

--- app/src/main/java/com/example/moon/bblind/MainActivity.kt
+++ app/src/main/java/com/example/moon/bblind/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.moon.bblind
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+        val myRef : DatabaseReference = database.getReference("new")
+        myRef.setValue("test")
+
+        button.setOnClickListener(View.OnClickListener
+        {
+
+            myRef.setValue(editText.text.toString())
+
+        })
+
     }
 }
